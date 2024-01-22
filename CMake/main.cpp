42e6bd7037c432bae3a0d8f5b77c6ee658456329
RitterAntoine <antoine.ritter@viacesi.fr>
2024-01-22 17:16:29 +0100

    feat: LibSL imported and working

--- CMake/main.cpp
+++ CMake/main.cpp
@@ -1,9 +1,17 @@
-// Simple exemple to print a message on the console
-
 #include <iostream>
+#include <LibSL/Math/Vertex.h>
 
-int main()
-{
-    std::cout << "Hello World!" << std::endl;
+int main() {
+
+    // Create a 2D double vector using the v2d type
+    LibSL::Math::v2d myVertex = LibSL::Math::V2D(9.4, 2.7);
+
+    // Access the components of the vector
+    double x = myVertex[0];
+    double y = myVertex[1];
+
+    // Print the components of the vector
+    std::cout << "x: " << x << ", y: " << y << std::endl;
+
     return 0;
 }
