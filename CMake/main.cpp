7a44f3ff121154a0eb93a16e980a3daff26cc1f1
RitterAntoine <antoine.ritter@viacesi.fr>
2024-01-22 17:47:07 +0100

    feat: 3D float

--- CMake/main.cpp
+++ CMake/main.cpp
@@ -4,7 +4,7 @@
 int main() {
 
     // Create a 2D double vector using the v2d type
-    LibSL::Math::v2d myVertex = LibSL::Math::V2D(9.4, 2.7);
+    LibSL::Math::v2d myVertex = LibSL::Math::V2D(1.0, 2.0);
 
     // Access the components of the vector
     double x = myVertex[0];
@@ -12,6 +12,17 @@ int main() {
 
     // Print the components of the vector
     std::cout << "x: " << x << ", y: " << y << std::endl;
+
+    // Create a 3D float vector using the v3f type
+    LibSL::Math::v3f myVertex2 = LibSL::Math::V3F(1.0f, 2.0f, 3.0f);
+
+    // Access the components of the vector
+    float x2 = myVertex2[0];
+    float y2 = myVertex2[1];
+    float z2 = myVertex2[2];
+
+    // Print the components of the vector
+    std::cout << "x: " << x2 << ", y: " << y2 << ", z: " << z2 << std::endl;
 
     return 0;
 }
